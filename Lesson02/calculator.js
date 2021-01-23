1d953366137dba051421f2717684a21da7748aba
eddy vazquez <>
2021-01-23 07:29:27 -0800

    Equations added

---
+++ Lesson02/calculator.js
@@ -0,0 +1,9 @@
+let numA = 22;
+let numB = 3;
+let numC = 16;
+let numD = 7;
+
+console.log((numB * numA) / numB);
+console.log((numC ** numD) % numA - numB + numD);
+console.log((--numA + numD) / ++numB);
+console.log((numD % numB) - numC);
