136efb40972e997ce27480c72d9a1c00f97a1a50
eddy vazquez <>
2021-02-07 22:41:19 -0800

    Assignment created

---
+++ lesson04/testScript.js
@@ -0,0 +1,11 @@
+function getNum() {
+  let userNum = prompt('I lied, but I will do some math for you. Give me a number and I will square it for you.');
+  userNum = Math.pow(userNum, 2);
+  alert('The square is: ' + userNum);
+}
+
+function getMultiples() {
+  let userNumTwo = prompt('What is the first number?')
+  let userNumThree = prompt('What is the second number?');
+  alert('The total is: ' + userNumTwo * userNumThree);
+}
