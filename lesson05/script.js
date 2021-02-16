3a485cbd5abed67f5ca78a2d219fd29bad06e151
eddy vazquez <>
2021-02-15 20:39:42 -0800

    Added overdraft protections

--- lesson05/script.js
+++ lesson05/script.js
@@ -5,45 +5,50 @@ let userAccount = 100;
 
 function asker() {
   userPrompt = prompt('Is there anything else we can help you with?');
-  action = userPrompt.toUpperCase();
-  return action;
+  return action = userPrompt.toUpperCase();
 }
 
-
-// maybe put the while out here
 while (action != 'Q') {
   switch (action) {
     //Withdrawals
     case 'W':
       let w = prompt('How much do you want to withdraw?');
-      userAccount = userAccount - parseInt(w);
-      alert('Your account balance is: ' + userAccount);
-      asker();
-      // userPrompt = prompt('Is there anything else we can help you with?');
-      // action = userPrompt.toUpperCase();
-      break;
+      let wNumber = parseInt(w);
+
+      function withdrawGuard() {
+        if (wNumber > userAccount) {
+          alert('Insufficient funds!');
+          return false;
+        } else {
+          return true;
+        }
+      }
+
+      if (withdrawGuard()){
+        userAccount = userAccount - wNumber;
+        alert('Your account balance is: ' + userAccount);
+        asker();
+        break;
+      } else {
+        asker();
+        break;
+      }
     //Deposit
     case 'D':
       let d = prompt('How much do you want to deposit?');
       userAccount = userAccount + parseInt(d);
       alert('Your account balance is: ' + userAccount);
       asker();
-      // userPrompt = prompt('Is there anything else we can help you with?');
-      // action = userPrompt.toUpperCase();
       break;
     //ViewBalance
     case 'B':
       alert('Your total balance is: ' + userAccount);
       asker();
-      // userPrompt = prompt('Is there anything else we can help you with?');
-      // action = userPrompt.toUpperCase();
       break;
     //Error Recovery
     default:
       alert('Unrecognized command. W - Withdraw, D - Deposit, B - View Balance, Q - Quit');
       asker();
-      // userPrompt = prompt('Let us try again! How can we help you today?');
-      // action = userPrompt.toUpperCase();
       break;
     };
 }
